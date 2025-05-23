4050fbe21aa8c34eb1fe4dd474d19a15d2ddfc1b
palminaN <albanmelget@icloud.com>
2025-05-23 18:08:58 +0200

    update mainActivity ajout champs pour retrofit

--- app/src/main/java/com/example/edullm/MainActivity.java
+++ app/src/main/java/com/example/edullm/MainActivity.java
@@ -7,12 +7,43 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
+
+    Boolean connected;
+    Retrofit retrofit;
+
+    EduAPI apiService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.setConnected(false);
         setContentView(R.layout.main_layout);
-        Log.d("test", "main loaded");
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://edullm.onrender.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        apiService = retrofit.create(EduAPI.class);
+    }
+
+
+
+    public Boolean isConnected() {
+        return connected;
+    }
+
+
+    public void setConnected(Boolean state){
+
+        this.connected = state;
+
+    }
+
+
+    public EduAPI getApiService() {
+        return apiService;
     }
 }
