559005398d1ff9020a0d74a6ba1084397c74ff50
MelukHa <lucas.rebagliato@gmail.com>
2025-05-23 17:10:09 +0200

    essais render

--- server/main.py
+++ server/main.py
@@ -169,3 +169,15 @@ def update_child_account(child_id: int, updates: schemas.UserUpdate, db: Session
     db.commit()
     db.refresh(user)
     return user
+
+
+
+
+
+
+if __name__ == "__main__":
+    import os
+    import uvicorn
+
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
