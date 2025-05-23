5b74512b0516df0911cc580f86b67104f427380c
palminaN <albanmelget@icloud.com>
2025-05-23 18:28:15 +0200

    ajout material component au build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation("androidx.navigation:navigation-ui:2.7.7")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.google.android.material:material:1.11.0")
 
     implementation(libs.appcompat)
     implementation(libs.material)
