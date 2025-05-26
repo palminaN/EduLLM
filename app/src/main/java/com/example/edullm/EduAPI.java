614edfa55c085d5a1c531bb78fbcb166a968f4e3
palminaN <albanmelget@icloud.com>
2025-05-26 18:24:55 +0200

    ajout de routes a l'interface de l'api

--- app/src/main/java/com/example/edullm/EduAPI.java
+++ app/src/main/java/com/example/edullm/EduAPI.java
@@ -3,14 +3,13 @@ package com.example.edullm;
 import com.example.edullm.Models.ChildProfile;
 import com.example.edullm.Models.LangueExercise;
 import com.example.edullm.Models.LangueExerciseResult;
-import com.example.edullm.Models.LangueExerciseSubmission;
 import com.example.edullm.Models.MathExercise;
 import com.example.edullm.Models.ParentProfile;
 import com.example.edullm.Models.QuizExercise;
 import com.example.edullm.Models.StateModel;
 import com.example.edullm.Models.RegisterLoginRequest;
 import com.example.edullm.Models.StorySession;
-import com.example.edullm.Models.UserID;
+import com.example.edullm.Models.User;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -26,24 +25,25 @@ public interface EduAPI {
     Call<StateModel> ping();
 
     @POST("register/")
-    Call<UserID> createUser(@Body RegisterLoginRequest u);
+    Call<User> createUser(@Body RegisterLoginRequest u);
 
     @POST("login/")
-    Call<UserID> connect(@Body RegisterLoginRequest u);
+    Call<User> connect(@Body RegisterLoginRequest u);
     @GET("child/{child_id}/profile")
     Call<ChildProfile> getChildProfile(@Path("child_id") long childId);
 
     @GET("parent/{parent_id}/profile")
     Call<ParentProfile> getParentProfile(@Path("parent_id") long parentId);
 
-    @GET("exercice/math")
+    @GET("exercise/math")
     Call<MathExercise> getMathExercises();
 
-    @GET("exercice/langue")
+    @GET("exercise/langue")
     Call<LangueExercise> getLangueExercises();
 
-    @POST("exercice/langue")
-    Call<LangueExerciseResult> submitLangueExercise(@Body LangueExerciseSubmission submission);
+    @POST("exercise/langue")
+    Call<LangueExerciseResult> submitLangueExercise(@Query("answer") String answer,
+                                                    @Query("instruction") String instruction);
 
     @POST("story/start")
     Call<StorySession> startStory(@Query("theme") String theme,
